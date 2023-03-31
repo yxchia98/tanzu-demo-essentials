e232c5c971b0142d06fc26d8aa80dfe6ea6acca3
Viktor <viktorvdberg@yahoo.com>
2023-03-31 09:53:30 +0200

    Update install.sh

--- tkg/install/install.sh
+++ tkg/install/install.sh
@@ -1,24 +1,26 @@
 #
 # TKG prerequisites installation script that works for Ubuntu 22.04.2
 #
-# Install & configure Docker
+# You need to download tanzu-cli-bundle-linux-amd64.tgz from VMware Customerconnect.
+# Copy the file to /home/$USER/tanzu in your VM
 #
-snap install docker
-sudo groupadd docker
-sudo usermod -aG docker viktor
+# tanzu-cli-bundle-linux-amd64.tgz 2.1   = Tanzu CLI v0.28.0
+# tanzu-cli-bundle-linux-amd64.tgz 2.1.1 = Tanzu CLI v0.28.1
 #
-#
-# Prompt user if the system should be rebooted
+# The tanzu-cli-bundle-linux-amd64.tgz filename doesn't tell which Tanzu CLI version is included.
+
+export TANZU_CLI_VERSION=v0.28.1
+
 #
 
-echo "You should reboot your system after you've installed Docker"
-echo "If you've already rebooted your system, select n to continue the script"
+read -p "Do you want to install Docker? Choose n if you've already installed Docker (y/n) " choice
 
-read -p "Do you want to reboot the system now? (y/n) " choice
-
-# Check user's response and reboot or continue with script
+# Check user's response, install & configure docker and reboot or continue with script
 if [[ $choice =~ ^[Yy]$ ]]; then
-    echo "Rebooting system..."
+    echo "Installing docker..."
+    snap install docker
+    sudo groupadd docker
+    sudo usermod -aG docker $USER
     sudo reboot
 else
     echo "Continuing with remaining steps of the script..."
@@ -27,14 +29,17 @@ fi
 
 #
 # Test docker
-# Optional - login to dockerhub: docker login --username <USERNAME>
 #
+# Optional - login to dockerhub if you face download rate limiting: 
+
+# docker login --username <USERNAME>
+
 # Test docker
 
 docker run hello-world
 
 #
-# Required configuration for Kind
+# Required configuration for Kind used by TKG installer
 #
 
 sudo sysctl net/netfilter/nf_conntrack_max=131072
@@ -43,36 +48,43 @@ sudo sysctl net/netfilter/nf_conntrack_max=131072
 # Install and configure Tanzu CLI
 #
 
+cd /home/$USER/tanzu
 tar xvzf tanzu-cli-bundle-linux-amd64.tar.gz
 cd cli/
-sudo install core/v0.28.0/tanzu-core-linux_amd64 /usr/local/bin/tanzu
+sudo install core/$TANZU_CLI_VERSION/tanzu-core-linux_amd64 /usr/local/bin/tanzu
 source <(tanzu completion bash)
+echo "source <(tanzu completion bash)" >> ~/.bashrc
 tanzu init
 tanzu version
 
 #
-# Install kubectl
-#
-
-sudo apt-get update
-sudo apt-get install -y ca-certificates curl
-sudo apt-get install -y apt-transport-https
-sudo curl -fsSLo /etc/apt/keyrings/kubernetes-archive-keyring.gpg https://packages.cloud.google.com/apt/doc/apt-key.gpg
-echo "deb [signed-by=/etc/apt/keyrings/kubernetes-archive-keyring.gpg] https://apt.kubernetes.io/ kubernetes-xenial main" | sudo tee /etc/apt/sources.list.d/kubernetes.list
-sudo apt-get install -y kubectl
-
-#
-# Alternative installation using snap
-# snap install kubectl --classic
-#
-# Install brew package manager for most easy installation of Carvel Tools
+# Install brew package manager
 #
 
 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 (echo; echo 'eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"') >> /home/viktor/.profile
 eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"
 
-#Install carvell tools
+#
+# Install Kubernetes tooling
+#
+
+brew install kubectl kubectx kube-ps1 yq git
+
+#
+# Autocompletion stuff and setting the prompt
+#
+
+source <(kubectl completion bash)
+echo "source <(kubectl completion bash)" >> ~/.bashrc
+
+source "/home/linuxbrew/.linuxbrew/opt/kube-ps1/share/kube-ps1.sh"
+echo "source \"/home/linuxbrew/.linuxbrew/opt/kube-ps1/share/kube-ps1.sh\"" >> ~/.bashrc
+
+PS1='$(kube_ps1)'$PS1
+echo "PS1='\$(kube_ps1)'\$PS1"
+
+#Install Carvel tools
 
 brew tap vmware-tanzu/carvel
 brew install ytt kbld kapp imgpkg kwt vendir kctrl
